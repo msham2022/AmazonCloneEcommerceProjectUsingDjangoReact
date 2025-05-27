ee5a4a4e4da1d7b251a2d794e1930659cfbf39c1
Mohammad Shamsuddin <mshamsuddin.it@gmail.com>
2025-05-27 12:10:12 +0530

    change the env secrets

--- Backend/EcommerceInventory/EcommerceInventory/settings.py
+++ Backend/EcommerceInventory/EcommerceInventory/settings.py
@@ -100,11 +100,11 @@ WSGI_APPLICATION = 'EcommerceInventory.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+        'NAME': os.getenv('ecommerce'),
+        'USER': os.getenv('ecommerce'),
+        'PASSWORD': os.getenv('ecommerce'),
+        'HOST': os.getenv('localhost'),
+        'PORT': os.getenv('3306'),
     }
 }
 
